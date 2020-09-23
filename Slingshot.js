9533693dc41ac191cdcb4cb9ea8c3df2264bdcf1
Vedika <vedikabinani550@gmail.com>
2020-09-23 18:49:42 +0530

    added slingshot

---
+++ Slingshot.js
@@ -0,0 +1,17 @@
+class Slingshot{
+    constructor(bodyA, bodyB){
+        var options={
+            bodyA : bodyA,
+            bodyB : bodyB,
+            stiffness : 0.04,
+            length : 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    display(){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.bodyB.position;
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundimg;
 var platform;
+var slingshot;
 
 function preload(){
     backgroundimg = loadImage("sprites/bg.png");
@@ -34,9 +36,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,150, PI/2);
 
     bird = new Bird(100,100);
 
+    slingshot = new Slingshot(bird.body,log6.body);
 }
 
 function draw(){
@@ -60,6 +64,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
+
+   slingshot.display();
 }
